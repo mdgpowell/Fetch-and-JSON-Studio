c4f4a377dbcd74cb046a42830b49c559e520641a
Marcus Powell <marcuspowell@MacBook-Pro-4.local>
2022-11-30 13:40:13 -0600

    late commit

--- script.js
+++ script.js
@@ -9,9 +9,10 @@
 // })
 
 window.addEventListener("load", function() {
+    let data;
     const fetchData = async () => {
         const response = await fetch("https://handlers.education.launchcode.org/static/astronauts.json");
-        const data = await response.json();
+        data = await response.json();
         console.log(data);
 
         const container = document.getElementById("container");
@@ -26,18 +27,18 @@ window.addEventListener("load", function() {
             }
             return result
         }
-        for (i =0; i<data.length; i++) {
+        for (element of data) {
             container.innerHTML += `
             <div class="astronaut">
             <div class="bio">
-               <h3>${data[i].firstName} ${data[i].lastName}</h3>
+               <h3>${element.firstName} ${element.lastName}</h3>
                <ul>
-                  <li>Hours in space: ${data[i].hoursInSpace}</li>
-                  <li>Active: ${data[i].active}</li>
-                  <li>Skills: ${formatString(data[i].skills)}</li>
+                  <li>Hours in space: ${element.hoursInSpace}</li>
+                  <li>Active: ${element.active}</li>
+                  <li>Skills: ${formatString(element.skills)}</li>
                </ul>
             </div>
-            <img class="avatar" src="${data[i].picture}">
+            <img class="avatar" src="${element.picture}">
          </div>`
         }
     }
